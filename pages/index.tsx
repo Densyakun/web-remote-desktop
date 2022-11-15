aa98ca6b926c652a2384c36cb6b063954ce17230
densyakun <densyakun1128@gmail.com>
2022-11-15 18:50:33 +0900

    move head link to document

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,6 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Web Remote Desktop</title>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
       </Head>
 
       {user?.isLoggedIn === true && (user?.loginPasswordIsExist ?? false) &&
